10ec9152d9c88593547e92a58aa7026ba8360517
Jason Urban <35979170+JasonAbsolute@users.noreply.github.com>
2025-02-07 07:17:28 -0600

    Added alot of things oh god (#3)
    Co-authored-by: Jaysonn <urbanj@msoe.edu>

--- scripts/marioHUD.cs
+++ scripts/marioHUD.cs
@@ -4,18 +4,23 @@ using System;
 public partial class marioHUD : Node2D
 {
 	// Called when the node enters the scene tree for the first time.
-	Label Hud;
+	Label marioState;
+	Label currentAnimation;
 	Mario player;
+	AnimationPlayer animationPlayer;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Hud = GetNode<Label>("marioState");
+		marioState = GetNode<Label>("marioState");
+		currentAnimation = GetNode<Label>("currentAnimation");
 		player = GetParent().GetNode<Mario>("Mario");
+		animationPlayer = GetParent().GetNode<Mario>("Mario").GetNode<AnimationPlayer>("AnimationPlayer");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Hud.Text = player.stateOfMario.ToString();
+		marioState.Text = "Mario's State: " + player.stateOfMario.ToString();
+		currentAnimation.Text = "Current Animation: " + animationPlayer.CurrentAnimation;
 	}
 }
